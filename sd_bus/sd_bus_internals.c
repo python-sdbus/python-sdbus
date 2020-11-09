5c2bb4bb77c70e4daa063d3e3fbb9bd814e6fbb8
igo95862 <igo95862@yandex.ru>
2020-11-09 11:11:45 +0300

    Added license

--- sd_bus/sd_bus_internals.c
+++ sd_bus/sd_bus_internals.c
@@ -1,3 +1,23 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/*
+    Copyright (C) 2020  igo95862
+
+    This file is part of py_sd_bus
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <systemd/sd-bus.h>

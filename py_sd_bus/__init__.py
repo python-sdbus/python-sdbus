4c737fcde6b98d21510b38f4b0ce57be00a6b50d
igo95862 <igo95862@yandex.ru>
2020-11-26 19:20:12 +0300

    get_default_bus is now exported to the module

--- py_sd_bus/__init__.py
+++ py_sd_bus/__init__.py
@@ -17,3 +17,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+from .sd_bus_internals import get_default_bus
+
+__all__ = ['get_default_bus']

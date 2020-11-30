0f821f55ce81bdbc15461021d424418890d31d10
igo95862 <igo95862@yandex.ru>
2020-11-30 22:15:51 +0300

    test: Created an isolated dbus test case for future tests

---
+++ test/test_sd_bus.py
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+# Copyright (C) 2020  igo95862
+
+# This file is part of py_sd_bus
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+from __future__ import annotations
+
+from asyncio.subprocess import create_subprocess_exec
+
+from .common_test_util import TempDbusTest
+
+
+class TestPing(TempDbusTest):
+
+    async def test_ping_with_busctl(self) -> None:
+        busctl_process = await create_subprocess_exec(
+            '/usr/bin/busctl',
+            f'--address=unix:path={self.dbus_socket_path}',
+            'call',
+            'org.freedesktop.DBus', '/org/freedesktop/DBus',
+            'org.freedesktop.DBus.Peer', 'Ping',
+        )
+        return_code = await busctl_process.wait()
+        self.assertEqual(return_code, 0)

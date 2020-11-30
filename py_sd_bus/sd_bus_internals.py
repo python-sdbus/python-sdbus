f54a426a35b7e5837710887ef3e2c29863071acd
igo95862 <igo95862@yandex.ru>
2020-11-30 17:21:28 +0300

    Started with python interface

---
+++ py_sd_bus/sd_bus_internals.py
@@ -0,0 +1,132 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+# Copyright (C) 2020  igo95862
+
+# This file is part of py_sd_bus
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+from __future__ import annotations
+from typing import Sequence, Union, Tuple, Dict, Any, List
+from asyncio import Queue, Future
+
+
+DbusBasicTypes = Union[str, int, bytes, float, Any]
+DbusStructType = Tuple[DbusBasicTypes, ...]
+DbusDictType = Dict[DbusBasicTypes, DbusBasicTypes]
+DbusVariantType = Tuple[str, DbusStructType]
+DbusListType = List[DbusBasicTypes]
+DbusCompleteTypes = Union[DbusBasicTypes, DbusStructType,
+                          DbusDictType, DbusVariantType, DbusListType]
+
+
+class SdBusSlot:
+    """Holds reference to SdBus slot"""
+    ...
+
+
+class SdBusInterface:
+
+    def add_method(
+        self,
+        member_name: str,
+        signature: str, input_args_names: Sequence[str],
+        result_signature: str, result_args_names: Sequence[str],
+        flags: int, /
+    ) -> None:
+        ...
+
+
+class SdBusMessage:
+    def add_bytes_array(self, the_bytes: Union[bytes, bytearray], /) -> None:
+        ...
+
+    def append_basic(self, signature: str, *args: DbusBasicTypes) -> None:
+        ...
+
+    def open_container(self, container_type: str,
+                       container_signature: str, /) -> None:
+        ...
+
+    def close_container(self) -> None:
+        ...
+
+    def enter_container(self, container_type: str,
+                        container_signature: str, /) -> None:
+        ...
+
+    def exit_container(self) -> None:
+        ...
+
+    def dump(self) -> None:
+        ...
+
+    def seal(self) -> None:
+        ...
+
+    def get_contents(self
+                     ) -> Tuple[DbusCompleteTypes, ...]:
+        ...
+
+    def create_reply(self) -> SdBusMessage:
+        ...
+
+    def send(self) -> None:
+        ...
+
+
+class SdBus:
+    def call(self, message: SdBusMessage, /) -> None:
+        ...
+
+    async def call_async(
+            self, message: SdBusMessage,
+            /) -> Future[SdBusMessage]:
+        ...
+
+    def drive(self) -> None:
+        ...
+
+    def get_fd(self) -> int:
+        ...
+
+    def new_method_call_message(
+            self,
+            destination_name: str, object_path: str,
+            interface_name: str, member_name: str,
+            /) -> SdBusMessage:
+        ...
+
+    def add_interface(self, new_interface: SdBusInterface, /) -> None:
+        ...
+
+    def get_signal_queue_async(
+        self,
+        destination_name: str, object_path: str,
+        interface_name: str, member_name: str,
+        /
+    ) -> Future[Queue[SdBusMessage]]:
+        ...
+
+
+def get_default_bus() -> SdBus:
+    ...
+
+
+def encode_object_path(prefix: str, external: str) -> str:
+    ...
+
+
+def decode_object_path(prefix: str, full_path: str) -> str:
+    ...

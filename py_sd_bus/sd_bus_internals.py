953b9dedb98c18befd6595838afef439fd6c04dc
igo95862 <igo95862@yandex.ru>
2020-11-30 17:33:49 +0300

    Sorted imports

--- py_sd_bus/sd_bus_internals.py
+++ py_sd_bus/sd_bus_internals.py
@@ -18,9 +18,9 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 from __future__ import annotations
-from typing import Sequence, Union, Tuple, Dict, Any, List
-from asyncio import Queue, Future
 
+from asyncio import Future, Queue
+from typing import Any, Dict, List, Sequence, Tuple, Union
 
 DbusBasicTypes = Union[str, int, bytes, float, Any]
 DbusStructType = Tuple[DbusBasicTypes, ...]

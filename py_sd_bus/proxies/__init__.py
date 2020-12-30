02395dd3e11d3b5c9980b73c7cf65fcf57ae782d
igo95862 <igo95862@yandex.ru>
2020-12-30 14:19:21 +0300

    Started on notifications interface

--- py_sd_bus/proxies/__init__.py
+++ py_sd_bus/proxies/__init__.py
@@ -17,10 +17,10 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 from __future__ import annotations
 
-from typing import Optional, List, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 
-from ..dbus_proxy import (DbusInterfaceCommon, dbus_method,
-                          get_bus, dbus_property, DbusSignal)
+from ..dbus_proxy import (DbusInterfaceCommon, DbusSignal, dbus_method,
+                          dbus_property, dbus_signal, get_bus)
 from ..sd_bus_internals import SdBus
 
 
@@ -48,3 +48,53 @@ class FreedesktopDbus(DbusInterfaceCommon,
     name_lost: DbusSignal[str] = DbusSignal('NameLost')
     name_owner_changed: DbusSignal[Tuple[str, str, str]] = \
         DbusSignal('NameOwnerChanged')
+
+
+class NotificationsInterface(DbusInterfaceCommon,
+                             interface_name='org.freedesktop.Notifications'
+                             ):
+
+    @dbus_method(input_signature="u")
+    async def close_notification(self, notif_id: int) -> None:
+        raise NotImplementedError
+
+    @dbus_method()
+    async def get_capabilities(self) -> List[str]:
+        raise NotImplementedError
+
+    @dbus_method()
+    async def get_server_infomation(self) -> Tuple[str, str, str, str]:
+        raise NotImplementedError
+
+    @dbus_method(input_signature="susssasa{sv}i")
+    async def notify(
+            self,
+            app_name: str,
+            replaces_id: int,
+            app_icon: str,
+            summary: str,
+            body: str,
+            actions: List[str],
+            hints: Dict[str, Tuple[str, Any]],
+            expire_timeout: int, ) -> int:
+
+        raise NotImplementedError
+
+    @dbus_signal()
+    def action_invoked(self) -> Tuple[int, int]:
+        raise NotImplementedError
+
+    @dbus_signal()
+    def notification_closed(self) -> Tuple[int, int]:
+        raise NotImplementedError
+
+
+class FreedesktopNotifications(NotificationsInterface):
+    def __init__(self, bus: Optional[SdBus] = None) -> None:
+        super().__init__()
+        self.bus: SdBus = bus if bus is not None else get_bus()
+        self._connect(
+            self.bus,
+            'org.freedesktop.Notifications',
+            '/org/freedesktop/Notifications'
+        )

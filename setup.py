5be474f93b5d7644f2217b9b13d43ded1208f9d0
igo95862 <igo95862@yandex.ru>
2021-02-01 17:44:05 +0300

    Fixed missing module

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
                  'Based on sd-bus from libsystemd.'),
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='0.7.1',
+    version='0.7.2',
     url='https://github.com/igo95862/python-sdbus',
     author='igo95862',
     author_email='igo95862@yandex.ru',
@@ -52,7 +52,9 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    packages=['sdbus', 'sdbus.proxies', 'sdbus.async_proxies'],
+    packages=['sdbus',
+              'sdbus.proxies', 'sdbus.async_proxies',
+              'sdbus._proxies_common'],
     python_requires='>=3.8',
     ext_modules=[
         Extension(

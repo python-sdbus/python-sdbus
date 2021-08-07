83184d8c351ffce1f37f13e663c07692a723fa65
igo95862 <igo95862@yandex.ru>
2021-08-07 12:21:42 +0300

    Added meson targets to lint and format python code

---
+++ tools/run_py_linters.py
@@ -0,0 +1,70 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+# Copyright (C) 2020, 2021 igo95862
+
+# This file is part of python-sdbus
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+from __future__ import annotations
+
+from os import environ
+from pathlib import Path
+from subprocess import run
+from argparse import ArgumentParser
+
+
+source_root = Path(environ['MESON_SOURCE_ROOT'])
+build_dir = Path(environ['MESON_BUILD_ROOT'])
+
+tools_dir = source_root / 'tools'
+src_dir = source_root / 'src'
+test_dir = source_root / 'test'
+wheel_build_dir = source_root / 'wheel-build'
+
+all_python_dirs = [src_dir, tools_dir, test_dir, wheel_build_dir]
+
+
+def linter_main() -> None:
+    run(
+        args=('flake8', *all_python_dirs),
+        check=True,
+    )
+
+
+def formater_main() -> None:
+    ...
+
+
+def main() -> None:
+    parser = ArgumentParser()
+    parser.add_argument(
+        'mode',
+        choices=('lint', 'format'),
+    )
+
+    args = parser.parse_args()
+
+    mode = args.mode
+
+    if mode == 'lint':
+        linter_main()
+    elif mode == 'format':
+        formater_main()
+    else:
+        raise ValueError('Unknown mode', mode)
+
+
+if __name__ == '__main__':
+    main()

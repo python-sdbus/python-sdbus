8e04b4143e08709ccb3163657bec8891924ead38
igo95862 <igo95862@yandex.ru>
2021-08-28 17:25:42 +0300

    Added gen-from-connection command to the module
    Generates interfaces classes from live modules on dbus.
    Takes the service name and one or more objects paths to extract
    introspection and generate classes.

--- src/sdbus/__main__.py
+++ src/sdbus/__main__.py
@@ -27,7 +27,30 @@ from .interface_generator import (
     DbusInterfaceIntrospection,
     generate_async_py_file,
     interfaces_from_file,
+    interfaces_from_str,
 )
+
+
+def run_gen_from_connection(namespace: Namespace) -> None:
+    connection_name = namespace.connection_name
+    object_paths = namespace.object_paths
+
+    from .dbus_proxy_sync import DbusInterfaceCommon
+
+    if namespace.system:
+        from .dbus_common import set_default_bus
+        from .sd_bus_internals import sd_bus_open_system
+        set_default_bus(sd_bus_open_system())
+
+    interfaces: List[DbusInterfaceIntrospection] = []
+    for object_path in object_paths:
+        connection = DbusInterfaceCommon(connection_name, object_path)
+        itrospection = connection.dbus_introspect()
+        interfaces.extend(interfaces_from_str(itrospection))
+
+    print(
+        generate_async_py_file(
+            interfaces, namespace.no_imports_header))
 
 
 def run_gen_from_file(namespace: Namespace) -> None:
@@ -55,6 +78,34 @@ def generator_main() -> None:
     generate_from_file_parser.add_argument(
         '--no-imports-header', action='store_false', default=True,
         help="Do NOT include 'import' header",
+    )
+
+    generate_from_connection = subparsers.add_parser('gen-from-connection')
+    generate_from_connection.set_defaults(func=run_gen_from_connection)
+
+    generate_from_connection.add_argument(
+        'connection_name',
+        help=(
+            'Name of the service connection to extract introspection. '
+            "For example, 'org.freedesktop.systemd1' for systemd."
+        )
+    )
+    generate_from_connection.add_argument(
+        'object_paths',
+        nargs='+',
+        help=(
+            'Object paths that the introspection will be extracted. '
+            'One or more.'
+        )
+    )
+    generate_from_connection.add_argument(
+        '--no-imports-header', action='store_false', default=True,
+        help="Do NOT include 'import' header",
+    )
+    generate_from_connection.add_argument(
+        '--system',
+        help='Use system D-Bus instead of session.',
+        action='store_true',
     )
 
     args = main_arg_parser.parse_args()

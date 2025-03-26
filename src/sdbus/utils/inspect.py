d7973e4f44be7150035c59c33f130b8a69283db4
igo95862 <igo95862@yandex.ru>
2025-03-26 16:48:32 +0000

    Document sdbus.utils.inspect using autodoc
    Will make it easier to add typing overloads in the future.

--- src/sdbus/utils/inspect.py
+++ src/sdbus/utils/inspect.py
@@ -71,6 +71,30 @@ def inspect_dbus_path(
     obj: Union[DbusInterfaceBase, DbusInterfaceBaseAsync],
     bus: Optional[SdBus] = None,
 ) -> str:
+    """Return the D-Bus path of an object.
+
+    If called on a D-Bus proxy returns path of the proxied object.
+
+    If called on a local D-Bus object returns the exported D-Bus path.
+    If object is not exported raises ``LookupError``.
+
+    If called on an object that is unrelated to D-Bus raises ``TypeError``.
+
+    The object's path is inspected in the context of the given bus and if the
+    object is attached to a different bus the ``LookupError`` will be raised.
+    If the bus argument is not given or is ``None`` the default bus will be
+    checked against.
+
+    :param obj:
+        Object to inspect.
+    :param bus:
+        Bus to inspect against.
+        If not given or is ``None`` the default bus will be used.
+    :returns:
+        D-Bus path of the object.
+
+    *New in version 0.13.0.*
+    """
     if bus is None:
         bus = get_default_bus()
 

7b8b0e2d089114cde04101fd4e7be17dcb46ae5c
Arkadiusz Bokowy <arkadiusz.bokowy@gmail.com>
2025-06-12 16:49:23 +0200

    Iterate interfaces in the MRO reversed order
    Iterating the MRO in the reversed order will allow to add interfaces to
    the given object in a well-defined way - always starting from the base
    class. Such ordering will be compatible with adding interfaces manually
    one by one.

--- src/sdbus/dbus_proxy_async_interface_base.py
+++ src/sdbus/dbus_proxy_async_interface_base.py
@@ -295,7 +295,7 @@ class DbusInterfaceBaseAsync(metaclass=DbusInterfaceMetaAsync):
         cls,
     ) -> Iterator[tuple[str, DbusClassMeta]]:
 
-        for base in cls.__mro__:
+        for base in reversed(cls.__mro__):
             meta = DBUS_CLASS_TO_META.get(base)
             if meta is None:
                 continue
